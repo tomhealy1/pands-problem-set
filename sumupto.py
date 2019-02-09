d52470e7aa6f314730bdb7db506d4b31aa25d897
tomhealy1 <alfaone156@gmail.com>
2019-02-09 21:43:11 +0000

    Started solution to Sumupto

---
+++ sumupto.py
@@ -0,0 +1,14 @@
+#x = integer with input call
+x = int(input("Please enter a positive integer: "))
+
+#if condition for negative integer
+if (x < 0):
+    print("Enter a positve number")
+#if 0 then sum is = to 0
+else:
+    sum = 0 #start of range 
+#while the condition x greater 0 is true then, 
+    while(x > 0):
+        sum += x #adds all integers in range plus x
+        x -= 1
+    print("The sum is",sum)
