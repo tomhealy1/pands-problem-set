eeff812b2424381202e42d98695752dea6973808
tomhealy1 <alfaone156@gmail.com>
2019-02-16 20:29:05 +0000

    Added calendar module instead

--- begins-with-t.py
+++ begins-with-t.py
@@ -1,15 +1,17 @@
-#First import package for datetinme
+import calendar
 from datetime import datetime
-#Set condition True for if today begins with T
-x = datetime
 
-if  x == 1:
-    print("Yes - Today begins with a T")
-elif x == 4:
-    print("Yes - Today begins with a T")
+now = datetime.now()
+
+weekday = now.weekday()
+
+if weekday == 2:
+    print("Yay today begins with a T")
+elif weekday == 4:
+    print("Yay today begins with a T")
 else:
-    print("No - Today does not begin with a T")
-    
+    print("No today does not begin with a T")
+
 
 
 
