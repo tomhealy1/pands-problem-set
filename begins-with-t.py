7ccbe36c6cebecc996a42fafee52dea7489cff2a
tomhealy1 <alfaone156@gmail.com>
2019-02-16 20:38:55 +0000

    Added comments

--- begins-with-t.py
+++ begins-with-t.py
@@ -1,15 +1,15 @@
-import calendar
-from datetime import datetime
+import calendar #import calendar module
+from datetime import datetime #import datetime mod
 
-now = datetime.now()
+now = datetime.now() #what is today's date and time
 
-weekday = now.weekday()
+weekday = now.weekday() #weekday = today day
 
-if weekday == 2:
+if weekday == 1: #if today's day is equal to Tuesday, print
     print("Yay today begins with a T")
-elif weekday == 4:
+elif weekday == 4:#if today is equal to Thursday print
     print("Yay today begins with a T")
-else:
+else: #if today not equal to Tuesday or Thursday print else statement
     print("No today does not begin with a T")
 
 
