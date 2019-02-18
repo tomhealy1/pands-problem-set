ba9253c2d6affaaefe5256b196bb47552563a5e5
tomhealy1 <alfaone156@gmail.com>
2019-02-18 17:49:51 +0000

    Added first part of problem

--- divisor.py
+++ divisor.py
@@ -1,1 +1,9 @@
 #Solution to problem 3
+#Create rnage with range function
+for num in range(1000,10000):
+    #if true statement for the remainder for 6  ( Half way there :-)
+    if num % 6 == 0:
+        print(num)
+
+        
+       
