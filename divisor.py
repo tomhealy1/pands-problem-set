eb8233bc97e5047e3303e8077363a64b920e780b
tomhealy1 <alfaone156@gmail.com>
2019-02-21 08:30:55 +0000

    Added extra comments

--- divisor.py
+++ divisor.py
@@ -1,5 +1,5 @@
 #Solution to problem 3
-#Create rnage with range function
+#Create range with range function to specify nums between 1000 and 10000
 for num in range(1000,10000):
     #if true statement for the remainder for 6  ( Half way there :-)
     if num % 6 == 0 and num % 12 != 0:
