3e6f0a83f1c6e176a4e896f8b4972028809753fd
tomhealy1 <alfaone156@gmail.com>
2019-02-21 18:27:29 +0000

    Added while statement

--- collatz.py
+++ collatz.py
@@ -3,4 +3,13 @@ x = int(input("Please enter a positive integer: "))
 if x < 0:
     print("Please enter a positive number")
 else:
-    continue
+    pass
+
+while x > 1:
+    if x % 2 == 0:
+        x = x // 2
+    else:
+        x = 3 * x + 1
+    print(x)
+
+
