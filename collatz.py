c093dd1916b3e10f77257edfbf113a11149b737f
tomhealy1 <alfaone156@gmail.com>
2019-02-21 08:59:37 +0000

    Added more to it

--- collatz.py
+++ collatz.py
@@ -1,4 +1,6 @@
 #Solution to collatz.py problem
-x = int(input("Please enter a positive integer"))
-if x <= 0:
+x = int(input("Please enter a positive integer: "))
+if x < 0:
     print("Please enter a positive number")
+else:
+    if x 
