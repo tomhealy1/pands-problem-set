3b80fd89664ef05dbea8eaab22b5659f9e6585d9
tomhealy1 <alfaone156@gmail.com>
2019-02-25 19:00:32 +0000

    Played with flow control and found simple way

--- prime.py
+++ prime.py
@@ -1,1 +1,16 @@
-#Solution 
+#Solution to problem 5 
+#Ask user for postive integer
+num = int(input("Please enter a positive integer: "))
+
+#x is equal to inputted number floor divided by 2
+x = num // 2
+
+#while x is greater than 1 
+while x > 1:
+    #if inputted number / x's remainder is 0 then the number cant be prime as
+    #as the it can be divided 
+        if num % x == 0:
+        print(num, "is not prime")
+        break
+else:
+    print(num , "is prime")
