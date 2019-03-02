b97e5d017aa4d114e88f0d6fc0303f5215c20787
tomhealy1 <alfaone156@gmail.com>
2019-03-02 22:42:01 +0000

    Added some code to graph

--- Problem-10.py
+++ Problem-10.py
@@ -1,1 +1,17 @@
 #Solution to Problem-10
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+
+
+for x in range(0,4,1):
+    y=x**2+2*x+2
+
+    #x= x+1
+
+fig= plt.figure()
+axes=fig.add_subplot(111)
+plt.show()
+
+
