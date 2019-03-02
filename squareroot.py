f0b00c211610b1bb859b0c1bb878bcda9d0a93dd
tomhealy1 <alfaone156@gmail.com>
2019-03-02 17:51:57 +0000

    Added comment and format

--- squareroot.py
+++ squareroot.py
@@ -1,7 +1,12 @@
 #Solution to Problem 7
-
+#import math mod to use square root function
 import math 
+#Ask user for input as float
 x = float(input("Please enter a positive number: "))
-
+#Add if condition for x greater that 0
 if x > 0:
-    print(math.sqrt(x))
+    z = math.sqrt(x) #square root value for x now becomes z
+    print("The square root of", x, "is approx.", "{:.1f}".format(z))
+#Added elif statement
+elif x < 0:
+    print("Please use a positive number")
