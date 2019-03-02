a111dd36194cc536998edbb00314ca72a627b0e9
tomhealy1 <alfaone156@gmail.com>
2019-03-02 14:11:25 +0000

    Started Problem 6

--- prime.py
+++ prime.py
@@ -1,16 +1,13 @@
 #Solution to problem 5 
 #Ask user for postive integer
 num = int(input("Please enter a positive integer: "))
-
-#x is equal to inputted number floor divided by 2
-x = num // 2
-
-#while x is greater than 1 
-while x > 1:
-    #if inputted number / x's remainder is 0 then the number cant be prime as
-    #as the it can be divided 
-        if num % x == 0:
-        print(num, "is not prime")
-        break
+#if the imputted number is great than 1
+if num > 1:
+    for i in range(2, num):     #create list of numbers between 2 and itself
+        if num % i == 0:   #if the remainder of num / any number in list is true to zero then it is not prime  
+            print(num , "is not prime")
+            break
+    else:
+        print(num, "is prime number")
 else:
-    print(num , "is prime")
+    print("Please enter a greater positive number")
---
+++ sescondstring.py
@@ -0,0 +1,6 @@
+#Solution to Problem No. 6
+
+#Variation of the int input request from user except asks for a string
+x = str(input("Please enter a sentence: "))
+print(x)
+
