3b66f16439e405026c352f5751daff6045ada68f
tomhealy1 <alfaone156@gmail.com>
2019-03-04 20:43:00 +0000

    Updated comments

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
 #Solution to problem 5 
 #Ask user for postive integer
 num = int(input("Please enter a positive integer: "))
-#if the imputted number is great than 1
+#if the inputted number is great than 1
 if num > 1:
     for i in range(2, num):     #create list of numbers between 2 and itself
         if num % i == 0:   #if the remainder of num / any number in list is true to zero then it is not prime  
