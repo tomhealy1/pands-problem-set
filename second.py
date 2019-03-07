c5affebcb5a4bd49166df2c94890a6d253d06df0
tomhealy1 <alfaone156@gmail.com>
2019-03-07 19:19:18 +0000

    Added weeke 7 lesson around the with as f function and message ststing closure of file

--- second.py
+++ second.py
@@ -1,14 +1,13 @@
 #Solution to Problem 9
-
-#Created a file with first few paragraphs of Moby-Dick 
-book = open('C:\\Users\\Teamwork\\Desktop\\Assessments\\pands-problem-set\\moby-dick.txt', 'r')
+#Added in Week 7 lessons, Created a file with first few paragraphs of Moby-Dick
+with open('C:\\Users\\Teamwork\\Desktop\\Assessments\\pands-problem-set\\moby-dick.txt', 'r') as book:
 #Uses readlines statement to assign the lines in book to new arugment book1 
-book1 = book.readlines()
+    book1 = book.readlines()
 #Same as secondstring problem, use the range and len function return every 2nd line
-for i in range(0, len(book1), 2):
-    print(book1[i])
-
-book.close()
+    for i in range(0, len(book1), 2):
+        print(book1[i])
+#UnIndented to add the message about the file closing
+print("The file has now closed")
 
 
 
