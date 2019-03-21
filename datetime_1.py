73f45741a98c4997f86c93a50f30684a8582c708
tomhealy1 <alfaone156@gmail.com>
2019-03-21 14:26:40 +0000

    Added more comments and checked it work

--- datetime_1.py
+++ datetime_1.py
@@ -1,9 +1,9 @@
 #Solution to Problem 8
 #Tom Healy
-#Need to rename as it was trying to import this file
+#Need to rename as it was trying to import this file, learned valuable lesson: Dont name your files the same as pkgs, mods or other python goodies!!
 import calendar
 from datetime import datetime
-#Same function as from Problem 2
+#Same function as from Problem 2, 
 now = datetime.now()
 #Renamed the newly formatted time to now1
 now1 = now.strftime("%A, %B %d %Y at %H:%M %p")
