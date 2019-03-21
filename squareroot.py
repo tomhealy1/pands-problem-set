9c7bcb9a4ac37171b405f12991ab4bde8d9faa05
tomhealy1 <alfaone156@gmail.com>
2019-03-21 14:20:27 +0000

    Cleaned up comments and explained the {:.1f}.format more

--- squareroot.py
+++ squareroot.py
@@ -2,11 +2,11 @@
 #Tom Healy
 #import math mod to use square root function
 import math 
-#Ask user for input as float
+#Asks user for input as float
 x = float(input("Please enter a positive number: "))
 #Add if condition for x greater that 0
 if x > 0:
-    z = math.sqrt(x) #square root value for x now becomes z
+    z = math.sqrt(x) #square root value for x now becomes z and formatted to one decimal place
     print("The square root of", x, "is approx.", "{:.1f}".format(z))
 #Added elif statement
 elif x < 0:
