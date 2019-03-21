7aec3d9a9674fa1b86cda311e823ca56b684917e
tomhealy1 <alfaone156@gmail.com>
2019-03-21 15:54:27 +0000

    Added more comments to secondstring.py

--- secondstring.py
+++ secondstring.py
@@ -9,7 +9,7 @@ y_words = str(input("Please enter a sentence: "))
 #split y_works into separate words using .split()
 words = y_words.split(' ')
 
-#indxes from zero and loops through every 2nd item in words starting from zero
+#Indexes from zero, strts counting from the 2nd member in the range and loops through every 2nd item in words starting from zero
 for i in range(1,len(words), 2):
     #prints i taken the list words
     print(words[i])
