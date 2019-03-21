f67e2254353f7cc5f5baf9955e24ea9d164d0e92
tomhealy1 <alfaone156@gmail.com>
2019-03-21 14:15:02 +0000

    Cleaned up comments and removed the re mod

--- secondstring.py
+++ secondstring.py
@@ -1,14 +1,15 @@
 #Solution to Problem No. 6
 #Tom Healy
 #Variation of the int input request from user except asks for a string
+#Import the string package and re (regular expression package)
 import string
-import re
+#import re I did not need this as I am not matching a regex
 y_words = str(input("Please enter a sentence: "))
 
 #split y_works into separate words using .split()
 words = y_words.split(' ')
 
-#loops through every 2nd item in words satrting from zero
+#indxes from zero and loops through every 2nd item in words starting from zero
 for i in range(1,len(words), 2):
     #prints i taken the list words
     print(words[i])
