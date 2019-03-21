c5c4fa167d35a532139991edbf2b20c3552adedb
tomhealy1 <alfaone156@gmail.com>
2019-03-21 14:00:20 +0000

    Cleaned up comments and fixed the wrong day

--- begins-with-t.py
+++ begins-with-t.py
@@ -1,17 +1,25 @@
 #Solution to Problem 2
 #Tom Healy
 import calendar #import calendar module
-from datetime import datetime #import datetime mod
 
-now = datetime.now() #what is today's date and time
+from datetime import datetime #import datetime mod from datetime package
 
-weekday = now.weekday() #weekday = today day
+#what is today's date and time abd assigning that to the varible now
+now = datetime.now() 
 
-if weekday == 1: #if today's day is equal to Tuesday, print
+#Assigning today's weekday to the var weekday
+weekday = now.weekday() 
+
+#if today's day is equal to Tuesday, print "Yay today begins with a T". I could have used an or statement instead of a elif
+if weekday == 1: 
     print("Yay today begins with a T")
-elif weekday == 4:#if today is equal to Thursday print
+
+#if today is equal to Thursday print "Yay today begins with a T"
+elif weekday == 3:
     print("Yay today begins with a T")
-else: #if today not equal to Tuesday or Thursday print else statement
+
+#if today not equal to Tuesday or Thursday print else statement
+else: 
     print("No today does not begin with a T")
 
 
