59721438f8668eedf88e3bff1d415eef2efd43f2
tomhealy1 <alfaone156@gmail.com>
2019-03-26 22:22:01 +0000

    Added sys.argv

--- second.py
+++ second.py
@@ -1,6 +1,11 @@
 #Solution to Problem 9
 #Tom Healy
 #Added in Week 7 lessons, Created a file with first few paragraphs of Moby-Dick, 'r' denotes read and assigns it to var book
+
+import sys
+
+print("The name of the file is: ", sys.argv[1])
+
 with open('C:\\Users\\Teamwork\\Desktop\\Assessments\\pands-problem-set\\moby-dick.txt', 'r') as book:
 #Uses readlines statement to assign the lines in book to new arugment book1 
     book1 = book.readlines()
